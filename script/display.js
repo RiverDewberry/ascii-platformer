b284d55331042af40172255ff3abe2c35e476ca4
RiverDewberry <rde80630@oconeeschools.org>
2024-11-08 11:12:38 -0500

    updated display.js

--- script/display.js
+++ script/display.js
@@ -18,8 +18,8 @@ function draw()
         for (let i = 0; i < imgData.data.length; i += 4)
         {
             imgData.data[i] = 0;
-            imgData.data[i + 1] = 0;
-            imgData.data[i + 2] = 0;
+            imgData.data[i | 1] = 0;
+            imgData.data[i | 2] = 0;
         }
     }
     
